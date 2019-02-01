2d808f9da81f88f1ec611c3ebcdce611cccb60cc
lukeburpee <lukeburpee@gmail.com>
2019-02-01 15:44:31 -0500

    fix codelist tests

--- test-expands/a-codelist/js/gen-codelist.test-copy/feathers-gen-code.js
+++
@@ -1,7 +0,0 @@
-
-// feathers-gen-code.js - Codelist
-
-// !module src1/index.**
-// !code: listening
-  // code block from src1/index.js
-// !end
--- test-expands/a-codelist/js/test-codelist.test-expected/feathers-gen-code.js
+++ test-expands/a-codelist/js/test-codelist.test-expected/feathers-gen-code.js
@@ -1,5 +1,6 @@
 
 // feathers-gen-code.js - Codelist
+
 // !module src1/index.**
 // !code: listening
   // code block from src1/index.js
